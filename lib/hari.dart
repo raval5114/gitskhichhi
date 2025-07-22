fa84a36e35d60cf45ab05c5ae926c3c17bc97768
Hari Raval <hariraval81@gmail.com>
2025-07-23 01:12:11 +0530

    created a hari file

---
+++ lib/hari.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class View123 extends StatelessWidget {
+  const View123({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
